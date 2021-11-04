34e0eb02a7287e37a13fe0d08b8fa32af23dc900
joaogularte <jov96@hotmail.com>
2021-11-04 09:01:22 -0300

    wip

--- index.py
+++ index.py
@@ -2,10 +2,10 @@
 Exemplo de entrada:
 
 {
-  "conjunto_de_estados": ["q0", "q1"],
-  "alfabeto": ["a", "b"],
+  "conjunto_de_estados": ["q0", "q1", "q2", "q3"],
+  "alfabeto": ["a", "b", "c", "d"],
   "estado_inicial": "q0",
-  "estados_finais": ["q1"],
+  "estados_finais": ["q3"],
   "transicoes_entre_estados": [{
     "estado": "q0",
     "le": "a",
@@ -20,6 +20,26 @@ Exemplo de entrada:
     "estado": "q1",
     "le": "b",
     "proximo_estado": ["q1"]
+  },
+  {
+    "estado": "q1",
+    "le": "ε",
+    "proximo_estado": ["q2"]
+  },
+  {
+    "estado": "q2",
+    "le": "c",
+    "proximo_estado": ["q2"]
+  },
+  {
+    "estado": "q2",
+    "le": "ε",
+    "proximo_estado": ["q3"]
+  },
+  {
+    "estado": "q3",
+    "le": "d",
+    "proximo_estado": ["q3"]
   }]
 }
 
@@ -31,7 +51,17 @@ Exemplo de entrada:
 
 
 """
+def obter_e_closures(estado, afnde, lista_e_closure):
+  for transicao in afnde["transicoes_entre_estados"]:
+    if transicao["le"] == "ε" and estado == afnde["estado_inicial"]:
+      lista_e_closure.append(estado)
+      lista_e_closure.append(transicao["proximo_estado"])
+      obter_e_closures(transicao["proximo_estado"], afnde, lista_e_closure)
+    elif estado == transicao["estado"] and transicao["le"] == "ε"
+      lista_e_closure.append(transicao["proximo_estado"])
+      obter_e_closures()
+    elif
 
-nested transicoes em vazio
 
 def remove_transicoes_vazias(afnde):
+  conjunto_de_estados = afnde["conjunto_de_estados"]
