b357a7fd00d21f5aba26eb42a2c40f73b65152b2
João Vitor Gularte <jov96@hotmail.com>
2021-11-07 22:36:54 -0300

    chore: wip

--- index.py
+++ index.py
@@ -51,17 +51,89 @@ Exemplo de entrada:
 
 
 """
+
+def remover_transicoes_vazias(afnde):
+  return obter_e_closures_por_estado(afnde)
+
+def obter_e_closures_por_estado(afnde):
+  e_closures_por_estados =  {}
+  conjunto_de_estados = afnde["conjunto_de_estados"]
+  for estado in conjunto_de_estados:
+    e_closures = obter_e_closures(estado, afnde, [])
+    e_closures_por_estados[estado] = e_closures
+    break
+
+  return e_closures_por_estados
+
 def obter_e_closures(estado, afnde, lista_e_closure):
   for transicao in afnde["transicoes_entre_estados"]:
-    if transicao["le"] == "ε" and estado == afnde["estado_inicial"]:
+
+    if estado in afnde["estados_finais"] and transicao["estado"] == transicao["proximo_estado"]:
+      print("primeiro if")
+      lista_e_closure.append(estado)
+    elif not transicao["le"] == "ε" and transicao["estado"] == transicao["proximo_estado"]:
+      print("segundo if")
+
+      break
+    elif transicao["le"] == "ε" and estado == transicao["estado"] and estado == afnde["estado_inicial"]:
+      print("terceiro if")
+
+      lista_e_closure.append(estado)
+      lista_e_closure = obter_e_closures(transicao["proximo_estado"], afnde, lista_e_closure)
+    elif transicao["le"] == "ε" and estado == transicao["estado"]:
+      print("quarto if")
+
+      lista_e_closure.append(estado)
+      lista_e_closure = obter_e_closures(transicao["proximo_estado"], afnde, lista_e_closure)
+    elif transicao["le"] == "ε" and estado == transicao["estado"] and transicao["proximo_estado"] in afnde["estados_finais"]:
+      print("quinto if")
+
       lista_e_closure.append(estado)
       lista_e_closure.append(transicao["proximo_estado"])
-      obter_e_closures(transicao["proximo_estado"], afnde, lista_e_closure)
-    elif estado == transicao["estado"] and transicao["le"] == "ε"
-      lista_e_closure.append(transicao["proximo_estado"])
-      obter_e_closures()
-    elif
+
+  return lista_e_closure
 
 
-def remove_transicoes_vazias(afnde):
-  conjunto_de_estados = afnde["conjunto_de_estados"]
+afnde = {
+  "conjunto_de_estados": ["q0", "q1", "q2", "q3"],
+  "alfabeto": ["a", "b", "c", "d"],
+  "estado_inicial": "q0",
+  "estados_finais": ["q3"],
+  "transicoes_entre_estados": [{
+    "estado": "q0",
+    "le": "a",
+    "proximo_estado": ["q0"]
+  },
+  {
+    "estado": "q0",
+    "le": "ε",
+    "proximo_estado": ["q1"]
+  },
+  {
+    "estado": "q1",
+    "le": "b",
+    "proximo_estado": ["q1"]
+  },
+  {
+    "estado": "q1",
+    "le": "ε",
+    "proximo_estado": ["q2"]
+  },
+  {
+    "estado": "q2",
+    "le": "c",
+    "proximo_estado": ["q2"]
+  },
+  {
+    "estado": "q2",
+    "le": "ε",
+    "proximo_estado": ["q3"]
+  },
+  {
+    "estado": "q3",
+    "le": "d",
+    "proximo_estado": ["q3"]
+  }]
+}
+
+print(remover_transicoes_vazias(afnde))
